64cc99b68b31574b418e5baf05546fd0206ff5a3
Julian Valencia <valenciajuliann@hotmail.com>
2022-10-11 15:24:25 -0500

    version estable del creador de grafos

--- codigo/dataframe.py
+++ codigo/dataframe.py
@@ -1,3 +1,19 @@
 import pandas as pd
 df = pd.read_csv(r'calles_de_medellin_con_acoso.csv', sep=";")
-print(df)
+def crearGrafo():
+    grafo = {}
+    for origin in uOrigin:
+        grafo[origin] = {}
+    for index in df.index:
+        if df['destination'][index] in grafo:
+            if df['oneway'][index]:
+                grafo[df['origin'][index]] = {df['destination'][index]:(df['length'][index],df['harassmentRisk'][index])}
+                grafo[df['destination'][index]] = {df['origin'][index]:(df['length'][index],df['harassmentRisk'][index])}
+            else: grafo[df['origin'][index]] = {df['destination'][index]:(df['length'][index],df['harassmentRisk'][index])}
+        else:
+            if df['oneway'][index]:
+                grafo[df['destination']] = {}
+                grafo[df['origin'][index]] = {df['destination'][index]:(df['length'][index],df['harassmentRisk'][index])}
+                grafo[df['destination'][index]] = {df['origin'][index]:(df['length'][index],df['harassmentRisk'][index])}
+            else: grafo[df['origin'][index]] = {df['destination'][index]:(df['length'][index],df['harassmentRisk'][index])}
+    return grafo
